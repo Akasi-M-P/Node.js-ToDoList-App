c836d038a9a26743616f6d2a8ef4ab9f1816c1cc
Mba96 <mbajameel90@gmail.com>
2023-02-03 21:29:41 +0000

    Run code on EJS template, pass data from webpage to server

--- app.js
+++ app.js
@@ -2,45 +2,45 @@ const express = require("express")
 const bodyParser = require("body-parser")
 
 const app = express()
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
 const port = 9000
 
+let items = [];
+
 app.get('/', (req, res) => {
 
     let today = new Date();
-    let currentDay = today.getDay();
-    let day = "" ;
+    
 
-    switch (currentDay) {
-        case 0:
-            day = "Sunday";
-            break;
-        case 1:
-            day = "Monday";
-            break;
-        case 2:
-            day = "Tuesday";
-            break;
-        case 3:
-            day = "Wednesday";
-            break;
-        case 4:
-            day = "Thursday";
-            break;
-        case 5:
-            day = "Frday";
-            break;
-        case 6:
-            day = "Saturday";
-            break;
-        default:           
-    }
+    let options = { 
+        weekday: 'long', 
+        day: 'numeric', 
+        month: 'long', 
+         };
 
-        res.render("list", {
-            kindOfDay: day});
+    let day = today.toLocaleDateString("en-US", options);
+
+
+
+        res.render("list", {kindOfDay: day, newItems: items});
    
 });
+
+
+app.post('/', (req, res) => {
+
+    let addToList = "";
+    let item = req.body.addToList; 
+
+   items.push(item)
+
+    res.render("list", {newItem: addToList});
+    res.redirect('/');
+
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
